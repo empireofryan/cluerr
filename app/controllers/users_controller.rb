bbe36f4f985834764932edc06ec6c70ea35bee51
empireofryan <rjohnson2001@gmail.com>
2016-02-26 13:28:02 -0500

    added subscriptions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,30 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
+  skip_before_action :verify_authenticity_token
+
   def info
     @subscription = current_user.subscription
+    if @subscription.active
+      @stripe_customer = Stripe::Customer.retrieve(@subscription.stripe_user_id)
+      @stripe_subscription = @stripe_customer.subscriptions.first
+    end
   end
 
-  def change
+  def cancel_subscription
+  #  @stripe_customer = Stripe::Customer.retrieve(@subscription.stripe_user_id)
+  #  @stripe_subscription = @stripe_customer.subscriptions.first
+
+  #  @stripe_subscription.delete
+  #  current_user.subscription.active = false
+   # current_user.subscription.save
+    redirect_to users_info_path
+  end
+
+  def charge
     token = params["stripeToken"]
     customer = Stripe::Customer.create(
       source: token,
-      plan: 'mysubscriptionlevel1',
+      plan: 'my_subscription_level1',
       email: current_user.email
       )
 
